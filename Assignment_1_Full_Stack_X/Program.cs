1274ebd7876d0899ce97fb4e3704103616ad9aad
Abdelrahman Al-Harasees <Axel Romanov 47@AXEL_ROMANOV>
2026-02-09 19:08:26 +0300

    Add project files.

---
+++ Assignment_1_Full_Stack_X/Program.cs
@@ -0,0 +1,54 @@
+﻿Console.WriteLine("Please Enter Recite Amount in JDs:");
+var reciteAmount = Convert.ToDecimal(Console.ReadLine());
+if (reciteAmount <= 0)
+{
+    Console.WriteLine("Please Enter a Valid Price!");
+    return;
+}
+else if (reciteAmount >= 500)
+{
+    Console.WriteLine();
+    Console.WriteLine("========================================");
+    Console.WriteLine("               SALES RECEIPT            ");
+    Console.WriteLine("----------------------------------------");
+    Console.WriteLine($"Date: {DateTime.Now:ddd dd-MM-yyyy}    Time: {DateTime.Now:hh:mm:ss tt}");
+    Console.WriteLine("----------------------------------------");
+    Console.WriteLine($"Subtotal:{reciteAmount} JDs");
+    Console.WriteLine($"Discount:{(reciteAmount - ((reciteAmount * 20) / 100))} JDs ({20}%)");
+    Console.WriteLine("----------------------------------------");
+    Console.WriteLine($"TOTAL:   {(reciteAmount - ((reciteAmount * 20) / 100))} JDs");
+    Console.WriteLine("========================================");
+    Console.WriteLine("Thank you for your purchase!");
+    return;
+}
+else if (reciteAmount >= 300 && reciteAmount <= 499)
+{
+    Console.WriteLine();
+    Console.WriteLine("========================================");
+    Console.WriteLine("               SALES RECEIPT            ");
+    Console.WriteLine("----------------------------------------");
+    Console.WriteLine($"Date: {DateTime.Now:ddd dd-MM-yyyy}    Time: {DateTime.Now:hh:mm:ss tt}");
+    Console.WriteLine("----------------------------------------");
+    Console.WriteLine($"Subtotal:{reciteAmount} JDs");
+    Console.WriteLine($"Discount:{(reciteAmount - ((reciteAmount * 10) / 100))} JDs ({10}%)");
+    Console.WriteLine("----------------------------------------");
+    Console.WriteLine($"TOTAL:   {(reciteAmount - ((reciteAmount * 10) / 100))} JDs");
+    Console.WriteLine("========================================");
+    Console.WriteLine("Thank you for your purchase!");
+}
+else
+{
+    Console.WriteLine();
+    Console.WriteLine("========================================");
+    Console.WriteLine("               SALES RECEIPT            ");
+    Console.WriteLine("----------------------------------------");
+    Console.WriteLine($"Date: {DateTime.Now:ddd dd-MM-yyyy}    Time: {DateTime.Now:hh:mm:ss tt}");
+    Console.WriteLine("----------------------------------------");
+    Console.WriteLine($"Subtotal:{reciteAmount} JDs");
+    Console.WriteLine($"Discount:{reciteAmount} JDs ({0}%)");
+    Console.WriteLine("----------------------------------------");
+    Console.WriteLine($"TOTAL:   {reciteAmount} JDs");
+    Console.WriteLine("========================================");
+    Console.WriteLine("Thank you for your purchase!");
+    return;
+}
